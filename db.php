770f13058964afcea638a67eda438fbcd4dc3998
Bagus Muhammad Wijaksono <106671990+baguswijaksono@users.noreply.github.com>
2024-10-29 20:01:12 +0700

    Update db.php

--- db.php
+++ db.php
@@ -1,1 +1,9 @@
-
+<?php
+$servername = "localhost";
+$username = "skibidi";
+$password = "skibidi";
+$dbname = "skibidi";
+$conn = new mysqli($servername, $username, $password, $dbname);
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
